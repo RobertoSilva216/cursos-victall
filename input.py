4b12adc9b90de0f258df308608826de5c84f03c7
Roberto <roberto.pereira@manyminds.com.br>
2024-09-22 22:12:15 -0300

    Uso de input para entrada de dados do usuário

---
+++ input.py
@@ -0,0 +1,8 @@
+nome = input("Digite seu nome:")
+idade = int(input("Qual sua idade ?"))
+email = input("Digite seu e-mail:")
+
+
+print(f"Olá {nome}, você tem {idade} anos e seu e-mail é {email}")
+
+
